76bf8b3f4c3b98cbdf9d9417b1c0a50f888bac3a
Tkendi <acg70159@gmail.com>
2020-09-22 13:49:23 +0900

    server axios parsing

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -1,17 +1,27 @@
 import Router from "koa-router";
 import client from "./client.js";
+import convert from "xml-js";
 
 const api = new Router();
 
 const URL = process.env.URL;
-console.log(URL);
+
+const date = new Date();
+let stdDate = `${date.getFullYear()}${("0" + (date.getMonth() + 1)).slice(
+  -2
+)}${("0" + date.getDate()).slice(-2)}`;
 
 api.get("/", async ctx => {
+  const info = {
+    data: []
+  };
+
   const data = await client.get(
-    `${URL}?API_KEY=${process.env.serviceKey}/TYPE=json`
+    `211.237.50.150:7080/openapi/{process.env.serviceKey}/json/Grid_20150827000000000227_1/1/10/ `
   );
 
-  console.log(data.data)
+  const xmlTojson = convert.xml2json(data.data, { compact: true, spaces: 4 });
+  console.log(xmlTojson);
 });
 
 export default api;
