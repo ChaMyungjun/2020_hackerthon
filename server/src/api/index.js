a025ade86a1debb9935b8e7e01c8086100e24291
Tkendi <acg70159@gmail.com>
2020-09-23 09:15:58 +0900

    front end change

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -10,34 +10,36 @@ let stdDate = `${date.getFullYear()}${("0" + (date.getMonth() + 1)).slice(
   -2
 )}${("0" + (date.getDate() - 1)).slice(-2)}`;
 
-console.log(process.env.serviceKey)
+console.log(process.env.serviceKey);
 
-api.get("/info" ,async (ctx) => {
-    const info = {
-        name: [],
-        smry: [],
-        category: [],
-        img: [],
-        sensitive_imge: []
-    }
+api.get("/info", async (ctx) => {
+  const info = {
+    name: [],
+    smry: [],
+    category: [],
+    img: [],
+    sensitive_imge: [],
+  };
 
-    const data = await client.get(`http://211.237.50.150:7080/openapi/${process.env.serviceKey}/json/Grid_20150827000000000226_1/1/10/`)
+  const data = await client.get(
+    `http://211.237.50.150:7080/openapi/${process.env.serviceKey}/json/Grid_20150827000000000226_1/1/10/`
+  );
 
-    const items = data.data.Grid_20150827000000000226_1.row
+  const items = data.data.Grid_20150827000000000226_1.row;
 
-    for (const keys in items) {
-        info.name[keys] = items[keys].RECIPE_NM_KO
-        info.smry[keys] = items[keys].SUMRY
-        info.category[keys] = items[keys].NATION_NM
-        info.img[keys] = items[keys].IMG_URL
-        info.sensitive_imge[keys] = items[keys].DET_URL
-    }
+  for (const keys in items) {
+    info.name[keys] = items[keys].RECIPE_NM_KO;
+    info.smry[keys] = items[keys].SUMRY;
+    info.category[keys] = items[keys].NATION_NM;
+    info.img[keys] = items[keys].IMG_URL;
+    info.sensitive_imge[keys] = items[keys].DET_URL;
+  }
 
-    ctx.body = info
-    return info
-})
+  ctx.body = info;
+  return info;
+});
 
-api.get("/recipe", async ctx => {
+api.get("/recipe", async (ctx) => {
   const info = {
     name: [],
     type: [],
@@ -48,36 +50,39 @@ api.get("/recipe", async ctx => {
     `http://211.237.50.150:7080/openapi/${process.env.serviceKey}/json/Grid_20150827000000000227_1/1/10/`
   );
 
-  const items = data.data.Grid_20150827000000000227_1.row
-  
+  const items = data.data.Grid_20150827000000000227_1.row;
+
   for (const keys in items) {
-      info.name[keys] = items[keys].IRDNT_NM
-      info.number[keys] = items[keys].IRDNT_CPCTY
-      info.type[keys] = items[keys].IRDNT_TY_NM
+    info.name[keys] = items[keys].IRDNT_NM;
+    info.number[keys] = items[keys].IRDNT_CPCTY;
+    info.type[keys] = items[keys].IRDNT_TY_NM;
   }
 
-  ctx.body = info
-  return info
+  ctx.body = info;
+  return info;
 });
 
 api.get("/process", async (ctx) => {
-    const info = {
-        process: [],
-        img: [],
-        tip: []
-    }
-    const data = await client.get(`http://211.237.50.150:7080/openapi/${process.env.serviceKey}/json/Grid_20150827000000000228_1/1/10/`)
+  const info = {
+    process: [],
+    img: [],
+    tip: [],
+  };
+  const data = await client.get(
+    `http://211.237.50.150:7080/openapi/${process.env.serviceKey}/json/Grid_20150827000000000228_1/1/10/`
+  );
 
-    const items = data.data.Grid_20150827000000000228_1.row
+  const items = data.data.Grid_20150827000000000228_1.row;
 
-    for (const keys in items) {
-        info.process[keys] = items[keys].COOKING_DC
-        info.img[keys] = items[keys].STRE_STEP_IMAGE_URL
-        info.tip[keys] = items[keys].STEP_TIP
-    }
+  for (const keys in items) {
+    info.process[keys] = items[keys].COOKING_DC;
+    info.img[keys] = items[keys].STRE_STEP_IMAGE_URL;
+    info.tip[keys] = items[keys].STEP_TIP;
+  }
 
-    ctx.body = info
-    return info
-})
+  ctx.body = info;
+  return info;
+});
 
 export default api;
+
